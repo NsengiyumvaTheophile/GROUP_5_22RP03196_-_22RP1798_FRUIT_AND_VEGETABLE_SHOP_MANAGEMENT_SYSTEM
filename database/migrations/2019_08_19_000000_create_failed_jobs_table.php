1f11883237b50c7d2c3204de5441c27763ddaa27
Jados088 <164474860+Jados088@users.noreply.github.com>
2025-04-14 20:46:13 +0200

    failed_job_table.php

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
+       //creation of table columns
         Schema::create('failed_jobs', function (Blueprint $table) {
             $table->id();
             $table->string('uuid')->unique();
